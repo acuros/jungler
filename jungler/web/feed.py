3727d657df46c191bfc8fa8bfc8801412f611b60
YukSeungChan <loup1788@gmail.com>
2014-11-01 21:11:09 +0900

    Feed list, detail views and templates added.

---
+++ jungler/web/feed.py
@@ -0,0 +1,20 @@
+#-*- coding: utf-8 -*-
+from flask import Blueprint, request, render_template
+
+from jungler.models.feed import Feed
+
+blueprint = Blueprint('feed', __name__, url_prefix='/feed')
+
+
+@blueprint.route('s')
+def feeds():
+    feed_list = Feed.query.order_by(Feed.id.asc()).all()
+    return render_template('feed_list.html', feeds=feed_list)
+
+
+@blueprint.route('s/<feed_id>')
+def feed_detail(feed_id):
+    feed = Feed.get_or_404(feed_id)
+    return render_template('feed_detail.html', feed=feed.serialize())
+
+
