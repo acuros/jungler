906908d1fbd0689879b87905322330dcf358de90
YukSeungChan <loup@pionworks.com>
2014-11-04 07:21:25 -0800

    Additional feed list duplicate bug fixed.

--- jungler/web/feed.py
+++ jungler/web/feed.py
@@ -9,7 +9,7 @@ blueprint = Blueprint('feed', __name__)
 
 @blueprint.route('/feeds')
 def feeds():
-    feed_list = Feed.query.order_by(Feed.write_time.desc()).limit(30).all()
+    feed_list = Feed.query.order_by(Feed.write_time.desc(), Feed.id.desc()).limit(30).all()
     return render_template('feed_list.html', feeds=feed_list)
 
 
@@ -20,7 +20,13 @@ def additional_feeds():
     last_feed = Feed.get(last_feed_id)
     if last_feed is None:
         return dict(status=dict(code='FAIL', reason=u'올바르지 않은 요청입니다.'))
-    feeds_ = Feed.query.filter(Feed.id != last_feed_id, Feed.write_time >= last_feed.write_time).order_by(Feed.write_time.desc()).limit(30).all()
+    feeds_ = Feed.query.filter(
+        Feed.id != last_feed.id,
+        Feed.write_time > last_feed.write_time
+    ).order_by(
+        Feed.write_time.desc(), Feed.id.desc()
+    ).limit(30).all()
+
     serialized_feeds = []
     for f in feeds_:
         serialized_feeds.append(f.serialize())
