9676d5c0d148a1d494c6762bff7c0a41b450fced
YukSeungChan <loup@pionworks.com>
2014-11-04 07:05:58 -0800

    키워드가 없는 피드를 삭제하는 task 추가. 전체 키워드에 대한 크롤링이 끝나면 키워드 없는 피드삭제 task 실행.

--- jungler/tasks.py
+++ jungler/tasks.py
@@ -1,12 +1,10 @@
 #-*- coding: utf-8 -*-
-from flask import g
-
 from celery.schedules import crontab
 from celery.task import periodic_task
 
-from jungler.ext import celery
+from jungler.ext import db, celery
 from jungler.bots import NaverBot
-from jungler.models.keyword import Keyword
+from jungler.models import Feed, Keyword, FeedKeywords
 
 
 @celery.task(name='feeds_of_keyword_crawl')
@@ -18,9 +16,19 @@ def feeds_of_keyword_crawl(keyword_id):
     bot.crawl(keyword=keyword)
 
 
+@celery.task(name='remove_not_used_feeds')
+def remove_not_used_feeds():
+    for feed in Feed.query.all():
+        row_count = FeedKeywords.query.with_entities(FeedKeywords.id).filter(FeedKeywords.feed_id == feed.id).count()
+        if row_count == 0:
+            feed.delete(commit=False)
+    db.session.commit()
+
+
 @periodic_task(run_every=crontab(minute='*/5'))
 def all_feeds_of_keyword_crawl():
     keywords = Keyword.query.all()
     bot = NaverBot()
     for keyword in keywords:
         bot.crawl(keyword=keyword)
+    remove_not_used_feeds.delay()
